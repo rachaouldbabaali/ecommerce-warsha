13d96002f4d1eb55ec1ebe839421693fcf74cd79
rachaouldbabaali <rachaould@gmail.com>
2025-11-27 19:06:32 +0100

    Add getproductById function and route

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,9 +1,21 @@
 // product related controller functions will be here
 
-import products from '../data/mockproductData.js';
+import products from "../data/mockproductData.js";
 
 const getProducts = (req, res) => {
   res.json(products);
 };
 
-export { getProducts };
+// get product by id
+const getProductById = (req, res) => {
+  const productId = parseInt(req.params.id); // get id from url params
+  const product = products.find((p) => p.id === productId);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
+
+
+export { getProducts, getProductById };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,13 @@
 // applicatin url : http://localhost:5000/api/products
 
 import express from 'express';
-import { getProducts } from '../controllers/productControllers.js';
+import { getProducts, getProductById } from '../controllers/productControllers.js';
 
 const router = express.Router();
 
-router.get ('/', getProducts);   // localhost:5000/api/products/
+router.get ('/', getProducts);   // localhost:5000/api/products
+
+//re.params.id
+router.get('/:id', getProductById); // localhost:5000/api/products/:id
 
 export default router;
