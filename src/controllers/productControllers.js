ea1a207be8a4f3b65bb00e854aca0219ce825914
rachaouldbabaali <rachaould@gmail.com>
2025-11-27 19:27:23 +0100

    Add add/put/delete routes

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -17,5 +17,67 @@ const getProductById = (req, res) => {
   }
 };
 
+// add a new product
+const addProduct = (req, res) => {
+  const newProduct = req.body;
+  newProduct.id = products.length + 1;
 
-export { getProducts, getProductById };
+  //check body has name, price, description, category
+  if (!newProduct.name) {
+    return res.status(400).json({ message: "Product name is required" });
+  }
+  if (!newProduct.price) {
+    return res.status(400).json({ message: "Product price is required" });
+  }
+  if (!newProduct.category) {
+    return res.status(400).json({ message: "Product category is required" });
+  }
+
+  if (isNaN(newProduct.price)) {
+    return res.status(400).json({ message: "Product price must be a number" });
+  }
+  if (newProduct.description.length > 200) {
+    return res.status(400).json({
+      message: "Product description must be less than 200 characters",
+    });
+  }
+
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+};
+
+// Put ,  update product
+
+const updateProduct = (req, res) => {
+  const productId = parseInt(req.params.id);
+  const productIndex = products.findIndex((p) => p.id === productId);
+  if (productIndex === -1) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+   const { name, price, description, category } = req.body;
+
+    if (name) products[productIndex].name = name; 
+    if (price) {
+      if (isNaN(price)) {
+        return res.status(400).json({ message: "Product price must be a number" });
+      }
+        products[productIndex].price = price;
+    }
+    if (description) products[productIndex].description = description;
+    if (category) products[productIndex].category = category;
+
+  res.json(products[productIndex]);
+};
+
+// delete product
+const deleteProduct = (req, res) => {
+  const productId = parseInt(req.params.id);
+  const productIndex = products.findIndex((p) => p.id === productId);
+  if (productIndex === -1) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+  products.splice(productIndex, 1);
+  res.json({ message: "Product deleted successfully" });
+};
+
+export { getProducts, getProductById, addProduct, updateProduct, deleteProduct };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,13 +1,25 @@
 // applicatin url : http://localhost:5000/api/products
 
-import express from 'express';
-import { getProducts, getProductById } from '../controllers/productControllers.js';
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
-router.get ('/', getProducts);   // localhost:5000/api/products
+router.get("/", getProducts); // localhost:5000/api/products
 
 //re.params.id
-router.get('/:id', getProductById); // localhost:5000/api/products/:id
+router.get("/:id", getProductById); // localhost:5000/api/products/:id
+
+router.post("/", addProduct); // localhost:5000/api/products
+
+router.put("/:id", updateProduct); // localhost:5000/api/products/:id
+
+router.delete("/:id", deleteProduct); // localhost:5000/api/products/:id
 
 export default router;
